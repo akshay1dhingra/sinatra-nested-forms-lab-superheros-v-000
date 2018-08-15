de91565b10c0598cb524a582cb0a933762a3030f
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-08-15 00:38:19 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,14 +1,4 @@
-require 'pry'
-class SuperHero
 
-  attr_reader :name, :power, :bio
-
-  HEROES = []
-
-  def initialize(params)
-    @name = params[:name]
-    @power = params[:power]
-    @bio = params[:bio]
-  end
-
+class SuperHero < ActiveRecord::Base
+  belongs_to :team 
 end
