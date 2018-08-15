4890819089fd56b8c54093e54a68041a7a5bedaa
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-08-15 00:45:38 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,3 +1,13 @@
 class SuperHero
 
+  attr_reader :name, :power, :bio 
+
+  SUPERHEROS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+  end
+
 end
